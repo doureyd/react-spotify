9ca98b59844ac4a5a9307c4ac27bbdece6a060c0
daynox <doureyd@gmail.com>
2018-11-21 14:21:34 -0500

    Setup redux

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import data from './data';
+import session from './session';
+
+export default combineReducers({
+  data,
+  session,
+});
---
+++ src/reducers/session.js
@@ -0,0 +1,8 @@
+const session = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default session;
